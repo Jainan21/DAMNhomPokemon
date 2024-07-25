48ffffe56cd6a4cca8ec940f9a30df3d5036c07d
HungMinh123 <166581892+HungMinh123@users.noreply.github.com>
2024-07-26 01:09:51 +0700

    26/7
    1:09

--- app/src/main/java/com/example/duanmaunhompokemon/AuthorBookDetails.java
+++ app/src/main/java/com/example/duanmaunhompokemon/AuthorBookDetails.java
@@ -1,7 +1,6 @@
 package com.example.duanmaunhompokemon;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,19 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class bookdetails extends AppCompatActivity {
+public class AuthorBookDetails extends AppCompatActivity {
 
-    Button btBefore, btMenu, btLike, btByBook;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.bookdetails);
+        setContentView(R.layout.activity_author_book_details);
 
-        btBefore = findViewById(R.id.btnBefore);
-        btMenu = findViewById(R.id.btnMenu);
-        btLike = findViewById(R.id.btnLike);
-        btByBook = findViewById(R.id.btnBuyBook);
     }
-
-
 }
