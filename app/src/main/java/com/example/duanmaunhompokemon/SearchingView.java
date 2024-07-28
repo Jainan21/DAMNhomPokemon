ba49a80b2aa6c7a5d0da8e162e89ff3fa9c463e2
Jainan21 <143272157+Jainan21@users.noreply.github.com>
2024-07-28 12:47:47 +0700

    search

--- app/src/main/java/com/example/duanmaunhompokemon/SearchingView.java
+++ app/src/main/java/com/example/duanmaunhompokemon/SearchingView.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -26,7 +28,7 @@ public class SearchingView extends BaseActivity {
     RecyclerView BookSearchingView;
     ArrayList<Book> listBook;
     SearchingAdapter adpSearching;
-
+    ImageView btnSearch;
     @SuppressLint("WrongViewCast")
 
     @Nullable
@@ -35,6 +37,8 @@ public class SearchingView extends BaseActivity {
         super.onCreate(savedInstanceState);
         setupActionBarAndBack(R.layout.activity_searching_view, "Tìm kiếm");
 
+        btnSearch = findViewById(R.id.btnSearch);
+        
         BookSearchingView = findViewById(R.id.layout_searching);
         BookSearchingView.setLayoutManager(new GridLayoutManager(this, 2));
         BookSearchingView.setHasFixedSize(true);
@@ -49,6 +53,13 @@ public class SearchingView extends BaseActivity {
 
         adpSearching = new SearchingAdapter(this, listBook);
         BookSearchingView.setAdapter(adpSearching);
+        
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SearchingView.this, "Tim kiem thanh cong", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
