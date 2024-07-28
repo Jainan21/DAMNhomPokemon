16b7ebfd188b630cabb299632b8dd7656fe72511
nvhoang257 <aashoang@gmail.com>
2024-07-28 16:11:55 +0700

    c
    c

--- app/src/main/java/com/example/duanmaunhompokemon/Adapter/ListAuthorAdapter.java
+++ app/src/main/java/com/example/duanmaunhompokemon/Adapter/ListAuthorAdapter.java
@@ -6,42 +6,48 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.duanmaunhompokemon.R;
 
 import java.util.List;
 
-public class ListAuthorAdapter extends RecyclerView.Adapter<ListAuthorAdapter.MyViewHolder> {
-    private List<String> mData;
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-        public ImageView imageView;
-        public TextView textView;
-        public MyViewHolder(View v) {
-            super(v);
-            imageView = v.findViewById(R.id.item_image);
-            textView = v.findViewById(R.id.item_text);
-        }
-    }
+public class ListAuthorAdapter extends RecyclerView.Adapter<ListAuthorAdapter.ViewHolder> {
+    private List<String> data;
+
     public ListAuthorAdapter(List<String> data) {
-        mData = data;
-    }
-    @NonNull
-    @Override
-    public ListAuthorAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_layout, parent, false);
-        return new MyViewHolder(v);
+        this.data = data;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ListAuthorAdapter.MyViewHolder holder, int position) {
-        holder.textView.setText(mData.get(position));
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String item = data.get(position);
+        holder.itemText.setText(item);
+
+        holder.itemNumber.setText(String.valueOf(position + 4));
     }
 
     @Override
     public int getItemCount() {
-        return mData.size();
+        return data.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView itemText;
+        public TextView itemNumber;
+        public ImageView itemImage;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            itemText = itemView.findViewById(R.id.item_text);
+            itemNumber = itemView.findViewById(R.id.item_number);
+            itemImage = itemView.findViewById(R.id.item_image);
+        }
     }
 }
