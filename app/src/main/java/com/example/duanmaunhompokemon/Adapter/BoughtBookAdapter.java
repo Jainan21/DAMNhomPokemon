5c36dbd339736a690ed64f39fe4e475260cf2724
anhphi1125 <166363173+anhphi1125@users.noreply.github.com>
2024-07-30 16:31:04 +0700

    bought book

--- app/src/main/java/com/example/duanmaunhompokemon/Adapter/BoughtBookAdapter.java
+++ app/src/main/java/com/example/duanmaunhompokemon/Adapter/BoughtBookAdapter.java
@@ -1,4 +1,102 @@
 package com.example.duanmaunhompokemon.Adapter;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class BoughtBookAdapter {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.duanmaunhompokemon.DAO.dbDAO;
+import com.example.duanmaunhompokemon.Model.Account;
+import com.example.duanmaunhompokemon.Model.Book;
+import com.example.duanmaunhompokemon.Model.Trade;
+import com.example.duanmaunhompokemon.R;
+
+import java.util.ArrayList;
+
+public class BoughtBookAdapter extends RecyclerView.Adapter<BoughtBookAdapter.BoughtBookViewHolder> {
+
+    private Context c;
+    private ArrayList<Book> listBook;
+    private OnItemClickListener listener;
+    private dbDAO dao;
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+        void onReadNowClick(int position);
+    }
+
+    public BoughtBookAdapter(Context c, ArrayList<Book> listBook) {
+        this.c = c;
+        this.listBook = listBook;
+        this.dao = new dbDAO(c);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public BoughtBookAdapter.BoughtBookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflate = LayoutInflater.from(c);
+        View view = inflate.inflate(R.layout.recycle_item_bought_book, parent, false);
+        return new BoughtBookAdapter.BoughtBookViewHolder(view, listener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BoughtBookViewHolder holder, int position) {
+        Book book = listBook.get(position);
+        holder.tvNameBookBought.setText(book.getTitle());
+        holder.tvAuthor1.setText(dao.getAuthorNameByBookId(book.getId_book()));
+        holder.tvTheLoai1.setText(dao.getBookCategoryById(book.getId_book()));
+
+        holder.btReadNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    int position = holder.getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        listener.onReadNowClick(position);
+                    }
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listBook.size();
+    }
+
+    public static class BoughtBookViewHolder extends RecyclerView.ViewHolder {
+        TextView tvAuthor1, tvTheLoai1, tvNameBookBought;
+        Button btReadNow;
+
+        public BoughtBookViewHolder(View itemView, final OnItemClickListener listener) {
+            super(itemView);
+            tvAuthor1 = itemView.findViewById(R.id.tvAuthor1);
+            tvTheLoai1 = itemView.findViewById(R.id.tvTheLoai1);
+            btReadNow = itemView.findViewById(R.id.btReadNow);
+            tvNameBookBought = itemView.findViewById(R.id.tvNameBookBought);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
 }
+
--- app/src/main/java/com/example/duanmaunhompokemon/Model/BtBought.java
+++ app/src/main/java/com/example/duanmaunhompokemon/Model/BtBought.java
@@ -1,22 +1,40 @@
 package com.example.duanmaunhompokemon.Model;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.duanmaunhompokemon.Adapter.BoughtBookAdapter;
+import com.example.duanmaunhompokemon.Adapter.LikeBookAdapter;
+import com.example.duanmaunhompokemon.DAO.dbDAO;
 import com.example.duanmaunhompokemon.R;
+import com.example.duanmaunhompokemon.ReadingBookView;
+import com.example.duanmaunhompokemon.bookdetails;
+import com.example.duanmaunhompokemon.boughtbook;
+
+import java.util.ArrayList;
 
 public class BtBought extends Fragment {
-    TextView tvAuthor1, tvTheloai1;
-    Button btReadNow;
-
+    RecyclerView rvBoughtBook;
+    dbDAO dao;
+    ArrayList<Book> list;
+    Integer user_id;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,9 +45,53 @@ public class BtBought extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.boughtbookcase, container, false);
-        tvAuthor1 = v.findViewById(R.id.tvAuthor1);
-        tvTheloai1 = v.findViewById(R.id.tvTheLoai1);
-        btReadNow = v.findViewById(R.id.btReadNow);
+        rvBoughtBook = v.findViewById(R.id.rvBoughtBook);
+
+        getUserID();
+
+        rvBoughtBook.setLayoutManager(new LinearLayoutManager(getContext()));
+        dao = new dbDAO(v.getContext());
+        loadBought();
+
         return v;
+    }
+
+    public void getUserID() {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("user", MODE_PRIVATE);
+        Integer userId = sharedPreferences.getInt("user_id", -1);
+        if (userId != -1) {
+            user_id = userId;
+        }
+    }
+
+    public void loadBought()
+    {
+        list = dao.getPurchasedBooksByAccountId(user_id);
+        if (list == null || list.isEmpty()) {
+            // Hiển thị thông báo nếu không có sách yêu thích
+            Toast.makeText(getContext(), "Không có sách đã mua nào", Toast.LENGTH_SHORT).show();
+        } else {
+            BoughtBookAdapter adapter = new BoughtBookAdapter(getContext(), list);
+            rvBoughtBook.setAdapter(adapter);
+
+            adapter.setOnItemClickListener(new BoughtBookAdapter.OnItemClickListener() {
+                @Override
+                public void onItemClick(int position) {
+                    Book select = list.get(position);
+                    Intent intent = new Intent(getContext(), boughtbook.class);
+                    intent.putExtra("id_book", select.getId_book());
+                    startActivity(intent);
+                }
+
+                @Override
+                public void onReadNowClick(int position) {
+                    Book select = list.get(position);
+                    Intent intent = new Intent(getContext(), ReadingBookView.class);
+                    intent.putExtra("id_book", select.getId_book());
+                    startActivity(intent);
+                }
+            });
+
+        }
     }
 }
